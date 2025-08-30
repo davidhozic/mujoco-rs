6f15ce8e6ecd4d0937effb0ab75ea39f5d15b0bb
David Hozic <davidhozic@gmail.com>
2025-08-30 02:47:55 +0200

    Viewer as an optional feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub mod wrappers;
 pub mod prelude;
-pub mod viewer;
 pub mod util;
+
+#[cfg(feature = "viewer")]
+pub mod viewer;
 
 #[allow(warnings)]
 pub mod mujoco_c;  // raw MuJoCo C and C++ bindings
