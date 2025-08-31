c9fd898a572d4c451c2fb7fe6d07864c6ed10bb5
David Hozic <davidhozic@gmail.com>
2025-09-01 00:28:41 +0200

    Build system improvements (#8)
    * Build system improvements
    
    * Update documentation field
    
    * Remove useless change check
    
    * Version bump
    
    * Fix variable

--- build.rs
+++ build.rs
@@ -1,46 +1,84 @@
-use bindgen::callbacks::TypeKind;
-use bindgen::callbacks::{ParseCallbacks, DeriveInfo};
+
 use std::path::PathBuf;
 use std::env;
-use std::fs;
 
 
-#[derive(Debug)]
-struct CloneCallback;
+#[cfg(feature = "ffi-regenerate")]
+mod build_dependencies {
+    use bindgen::callbacks::{ParseCallbacks, DeriveInfo};
+    use super::*;
+    use std::fs;
 
-impl ParseCallbacks for CloneCallback {
-    fn add_derives(&self, info: &DeriveInfo) -> Vec<String> {
-        let mut data = vec!["Clone".into()];
-        if info.name.starts_with("mjui") || info.name == "mjrRect_" {
-            data.push("Copy".into());
+    #[derive(Debug)]
+    struct CloneCallback;
+
+    impl ParseCallbacks for CloneCallback {
+        fn add_derives(&self, info: &DeriveInfo) -> Vec<String> {
+            let mut data = vec!["Clone".into()];
+            if info.name.starts_with("mjui") || info.name == "mjrRect_" {
+                data.push("Copy".into());
+            }
+
+            data
         }
+    }
 
-        if info.kind == TypeKind::Enum {
-            data.push("TryFromPrimitive".into());
-        }
 
-        data
+    pub(crate) fn generate_ffi() {
+        let output_path = PathBuf::from("./src/");
+        let current_dir = env::current_dir().unwrap();
+        let include_dir_mujoco = current_dir.join("src/cpp/include/mujoco");
+        let include_dir_simulate = include_dir_mujoco.join("viewer");
+
+        let bindings_mujoco = bindgen::Builder::default()
+            .header(include_dir_mujoco.join("mujoco.h").to_str().unwrap())
+            .header(include_dir_simulate.join("simulate.hpp").to_str().unwrap())
+            .header(include_dir_simulate.join("glfw_dispatch.hpp").to_str().unwrap())
+            .clang_arg("-std=c++20")
+            .clang_arg("-stdlib=libc++")
+            .clang_arg(format!("-I{}", current_dir.join("mujoco/build/_deps/glfw3-src/include/").display()))
+            .blocklist_item("std::tuple.*")
+            .allowlist_item("mj.*")
+            .allowlist_item("mujoco::.*")
+            .allowlist_item("new_simulate")
+            .allowlist_item("free_simulate")
+            .layout_tests(false)
+            .derive_default(false)
+            .opaque_type("std::.*")
+            .derive_copy(false)
+            .rustified_enum(".*")
+            .parse_callbacks(Box::new(CloneCallback))
+            .generate()
+            .expect("unable to generate MuJoCo bindings");
+
+        let outputfile_dir = output_path.join("mujoco_c.rs");
+        let mut fdata = bindings_mujoco
+            .to_string();
+
+        /* Extra adjustments */
+        fdata = fdata.replace("pub __lx: std_basic_string_value_type<_CharT>,", "pub __lx: std::mem::ManuallyDrop<std_basic_string_value_type<_CharT>>,");
+        // Remove extra Clone
+        let re = regex::Regex::new(r"#\[derive\((.*?Clone.*?), Clone,?(.*?)\)\]").unwrap();
+        fdata = re.replace_all(&fdata, "#[derive($1, $2)]").to_string();
+
+        fs::write(outputfile_dir, fdata).unwrap();
     }
 }
 
 
 
 fn main() {
-    println!("cargo:rerun-if-env-changed=DOCS_RS");
-    if std::env::var("DOCS_RS").map_or(false, |x| x == "y" || x == "1") {
+    if std::env::var("DOCS_RS").is_ok() {
         return;
     }
 
-
     /// Environmental variable which contains the path to the MuJoCo's build/lib/ directory.
     /// This mean for static linking, otherwise the dynamic MuJoCo library can just be installed and used.
-    const MUJOCO_STATIC_LIB_PATH_VAR: &str = "MUJOCO_STATIC_LINK_LIB";
-    const MUJOCO_DYN_LIB_PATH_VAR: &str = "MUJOCO_DYNAMIC_LINK_LIB";
-    const GENERATE_FFI: &str = "GENERATE_FFI";
+    const MUJOCO_STATIC_LIB_PATH_VAR: &str = "MUJOCO_STATIC_LINK_DIR";
+    const MUJOCO_DYN_LIB_PATH_VAR: &str = "MUJOCO_DYNAMIC_LINK_DIR";
 
     println!("cargo:rerun-if-env-changed={MUJOCO_STATIC_LIB_PATH_VAR}");
     println!("cargo:rerun-if-env-changed={MUJOCO_DYN_LIB_PATH_VAR}");
-    println!("cargo:rerun-if-env-changed={GENERATE_FFI}");
 
     let mujoco_lib_path= env::var(MUJOCO_STATIC_LIB_PATH_VAR);
 
@@ -77,46 +115,6 @@ fn main() {
     }
 
 
-
-    if !env::var(GENERATE_FFI).map_or(false, |v| v == "y" || v == "1") {
-        return;
-    }
-
-    let output_path = PathBuf::from("./src/");
-    let current_dir = env::current_dir().unwrap();
-    let include_dir_mujoco = current_dir.join("src/cpp/include/mujoco");
-    let include_dir_simulate = include_dir_mujoco.join("viewer");
-
-    let bindings_mujoco = bindgen::Builder::default()
-        .header(include_dir_mujoco.join("mujoco.h").to_str().unwrap())
-        .header(include_dir_simulate.join("simulate.hpp").to_str().unwrap())
-        .header(include_dir_simulate.join("glfw_dispatch.hpp").to_str().unwrap())
-        .clang_arg("-std=c++20")
-        .clang_arg("-stdlib=libc++")
-        .clang_arg(format!("-I{}", current_dir.join("mujoco/build/_deps/glfw3-src/include/").display()))
-        .blocklist_item("std::tuple.*")
-        .allowlist_item("mj.*")
-        .allowlist_item("mujoco::.*")
-        .allowlist_item("new_simulate")
-        .allowlist_item("free_simulate")
-        .layout_tests(false)
-        .derive_default(false)
-        .opaque_type("std::.*")
-        .derive_copy(false)
-        .rustified_enum(".*")
-        .parse_callbacks(Box::new(CloneCallback))
-        .generate()
-        .expect("unable to generate MuJoCo bindings");
-
-    let outputfile_dir = output_path.join("mujoco_c.rs");
-    let mut fdata = bindings_mujoco
-        .to_string();
-
-    // Extra adjustments
-    fdata = fdata.replace("pub __lx: std_basic_string_value_type<_CharT>,", "pub __lx: std::mem::ManuallyDrop<std_basic_string_value_type<_CharT>>,");
-    let re = regex::Regex::new(r"#\[derive\((.*?Clone.*?), Clone, (.*?)\)\]").unwrap();
-    fdata = re.replace_all(&fdata, "#[derive($1, $2)]").to_string();
-    fdata = "use num_enum::TryFromPrimitive;\n\n".to_string() + &fdata;
-
-    fs::write(outputfile_dir, fdata).unwrap();
+    #[cfg(feature = "ffi-regenerate")]
+    build_dependencies::generate_ffi();
 }
